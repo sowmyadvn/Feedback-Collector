3c1b60a576328ce0da22e3d7fa5b0557d3add582
SomiDeev <sowmyadvn.92@gmail.com>
2020-08-14 18:05:33 -0700

    Passport oauth initial code

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 // load express module into its own variable
 const express = require ('express'); 
 
+//load passport and passport-google strategy
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth20').Strategy;
+
+// connect passport with new passport google-strategy instance and pass configs between them
+passport.use(new googleStrategy()); 
 // creates a new express application
 const app = express(); 
-
 // route handler
 // app => register route handler, get => method to watch for incoming http requests
 // '/' => watch for requests to '/' directory, req => object representing the incoming request
 // res => object representing the response data, body of arrow function => send JSON
 // data "{hi:'there'}" as response 
+/*
 app.get('/',(req,res) => {
     res.send({hi : 'SomTej'});
 });
+*/
+
+app.get('/auth/google')
 
 // tells node to listen on port set by heroku during rum-time or default it to 5000 if not set
 const PORT = process.env.PORT || 5000;
